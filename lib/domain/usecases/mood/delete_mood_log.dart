f6175568fd28b6a1f106abdde1a6f0ecc1388750
CVVlizok <lizaagafonova8@gmail.com>
2025-12-12 20:18:55 +0300

    удаления

---
+++ lib/domain/usecases/mood/delete_mood_log.dart
@@ -0,0 +1,12 @@
+import '../../interfaces/mood_repository.dart';
+
+class DeleteMoodLog {
+  final MoodRepository _repository;
+
+  DeleteMoodLog(this._repository);
+
+  Future<void> call(String id) {
+    return _repository.deleteMoodLog(id);
+  }
+}
+
